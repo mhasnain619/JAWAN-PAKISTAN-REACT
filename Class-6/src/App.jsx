593c60b2f9d5e40eae452676fb5002766cc79512
Muhammad Hasnain <hasnain0893@gmail.com>
2025-01-04 19:15:56 +0500

    class 6

---
+++ Class-6/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import TextField from '@mui/material/TextField';
+import PrimarySearchAppBar from './AppBar';
+import DashboardLayoutBasic from './Dashboard';
+function App() {
+
+  return (
+    <>
+      <DashboardLayoutBasic />
+    </>
+  )
+}
+
+export default App
