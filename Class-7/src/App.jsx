0c01117bc37ab7fe2eff547100e05b1aef1ba333
Muhammad Hasnain <hasnain0893@gmail.com>
2025-01-05 19:02:56 +0500

    State Management

--- Class-7/src/App.jsx
+++ Class-7/src/App.jsx
@@ -1,16 +1,24 @@
 
+import { useState } from 'react'
 import './App.css'
-let userName = 'Anaintay'
-const ChangeName = () => {
-  userName = 'Anaintay changed'
-  document.getElementById('username').innerHTML = userName
-}
+
 function App() {
 
+  const [userName, userFunc] = useState('Hello World')
+  const updateName = () => {
+    userFunc('Hello World Again')
+  }
+  // console.log(state)
+
+  // let userName = 'Anaintay'
+  // const ChangeName = () => {
+  //   userName = 'Anaintay changed'
+  //   document.getElementById('username').innerHTML = userName
+  // }
   return (
     <>
       <h1 id='username'>{userName}</h1>
-      <button onClick={ChangeName}>Change Name</button>
+      <button onClick={updateName}>Change Name</button>
     </>
   )
 }
